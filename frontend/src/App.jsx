7d70f0c9af8987c065a93b1aaa25d141e1f06875
pallavi <pmdhakne2002@gmail.com>
2024-03-01 10:40:56 +0530

    clear previos quiz.jsx file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,25 @@
-import React from 'react'
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Toaster } from 'react-hot-toast';
+import { Toaster } from "react-hot-toast";
 
-import Register from './pages/registration/Register';
-import Login from './pages/login/Login';
-import Home from './pages/home/Home';
-import Quiz from './pages/quiz/Quiz';
+import Register from "./pages/registration/Register";
+import Login from "./pages/login/Login";
+import Home from "./pages/home/Home";
+import Quiz from "./pages/quiz/Quiz";
 function App() {
   return (
     <div>
       <BrowserRouter>
-      <Toaster position='top-center' reverseOrder={false} />
-       <Routes>
-       <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/quiz" element={<Quiz />} />
-       </Routes>
+        <Toaster position="top-center" reverseOrder={false} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/quiz" element={<Quiz />} />
+        </Routes>
       </BrowserRouter>
     </div>
-  )
+  );
 }
 
 export default App;
