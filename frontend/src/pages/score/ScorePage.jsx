e2898edf82df3b00ddcfcedf79789a0d18831027
pallavi <pmdhakne2002@gmail.com>
2024-03-11 20:34:05 +0530

    added score css and try again btn

--- frontend/src/pages/score/ScorePage.jsx
+++ frontend/src/pages/score/ScorePage.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import toast from "react-hot-toast";
 import { useLocation } from "react-router-dom"; // Import useLocation
 import "./style.css";
+import Quiz from "../quiz/Quiz";
+import { Link } from "react-router-dom";
 
 const calculatePercentageMessage = (score, totalQuestions) => {
   const per = (score * 100) / totalQuestions;
@@ -20,13 +22,23 @@ function ScorePage() {
   const location = useLocation(); // Use useLocation to access the location object
   const { score, totalQuestions } = location.state || {}; // Extract score and totalQuestions from state, providing an empty object as a fallback
 
+  const message = calculatePercentageMessage(score, totalQuestions);
+
   return (
-    <div>
+    <div className="container">
       <h1>Your Score</h1>
       <h3>
         {score}/{totalQuestions}
       </h3>
-      <p className="msg">{calculatePercentageMessage(score, totalQuestions)}</p>
+      <p className="msg">{message}</p>
+      {message === "You are Failed" && (
+        <div>
+          <h1>Try again</h1>
+          <Link className="start-btn" to="/quiz">
+            Start Quiz
+          </Link>
+        </div>
+      )}
     </div>
   );
 }
